57b1ae0ee05435802d9e74c8a4fea7b1195d369d
Orphique <matthewcahyadi52@gmail.com>
2025-11-18 14:42:44 +0800

    form

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@ function kirimPesan() {
     const encodedKodepos = encodeURIComponent(form.value.kodepos);
     const encodedBarang = encodeURIComponent(keranjang.value.map(item => `${item.barang} (${item.jumlah}xRp${item.hargaAsli})`).join(', '));
     const encodedTotalHarga = encodeURIComponent("Rp" + totalHarga.value);
-    const url = `https://docs.google.com/forms/d/e/1FAIpQLSc5WTiXTG0D6LhxYyitz7-liy2DsmP5Z3Ds7Zk30jVlqfzwlA/formResponse` +
+    const url = `https://forms.gle/n99cnEnbFXaCCZ4F9/formResponse` +
     `?entry.1397467379=${encodedNama}` +
     `&entry.1515742534=${encodedAlamat}` +
     `&entry.1477140648=${encodedKelurahan}` +
